e6d27ddc44b4a49dda9b0117164459fc4b6d9b99
Philip M. Gollucci <pgollucci@p6m7g8.com>
2021-03-27 19:08:34 -0400

    refactor(link): remove rendundant symlinks; echo for now

--- README.md
+++ README.md
@@ -47,7 +47,7 @@
 - p6df::modules::pgsql::deps()
 - p6df::modules::pgsql::external::brew()
 - p6df::modules::pgsql::home::symlink()
-- p6df::modules::pgsql::init()
+
 
 
 ## Author
--- init.zsh
+++ init.zsh
@@ -48,18 +48,8 @@ p6df::modules::pgsql::external::brew() {
 ######################################################################
 p6df::modules::pgsql::home::symlink() {
 
-  ln -sf $P6_DFZ_SRC_P6M7G8_DIR/p6df-db/share/pgsqlrc $P6_DFZ_DATA_DIR/.pgsqlrc
-}
-
-######################################################################
-#<
-#
-# Function: p6df::modules::pgsql::init()
-#
-#>
-######################################################################
-p6df::modules::pgsql::init() {
-
+  echo ln -sf $P6_DFZ_SRC_P6M7G8_DIR/p6df-db/share/pgsqlrc $P6_DFZ_DATA_DIR/.pgsqlrc
+#  ln -sf $P6_DFZ_SRC_P6M7G8_DIR/p6df-db/share/pgsqlrc $P6_DFZ_DATA_DIR/.pgsqlrc
 }
 
 # To migrate existing data from a previous major version of PostgreSQL run:
